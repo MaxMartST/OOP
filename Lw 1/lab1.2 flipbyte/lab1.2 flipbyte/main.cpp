3660f7d7ad77a3f0caf546678490f6dd4086bd26
MaxMartST <AdderGtx@gmail.com>
2020-02-27 23:07:40 +0300

    Исправил обработкик исключений Переименовал функции парсинга Изменил тип выводимого результата функции FlipByte

--- Lw 1/lab1.2 flipbyte/lab1.2 flipbyte/main.cpp
+++ Lw 1/lab1.2 flipbyte/lab1.2 flipbyte/main.cpp
@@ -13,11 +13,11 @@ void CheckRange(int number)
 {
 	if (std::numeric_limits<uint8_t>::max() < number || number < std::numeric_limits<uint8_t>::min())
 	{
-		throw std::exception("Invalid number range. Valid range is from 0 to 255");
+		throw std::out_of_range("Invalid number range. Valid range is from 0 to 255");
 	}
 }
 
-std::optional<Content> CheckArgument(int argc, char* argv[])
+std::optional<Content> ParsArgs(int argc, char* argv[])
 {
 	if (argc != 2)
 	{
@@ -25,7 +25,7 @@ std::optional<Content> CheckArgument(int argc, char* argv[])
 		return std::nullopt;
 	}
 	Content content;
-	content.inputString = argv[1];
+	content.inputString = argv[1];//сделать отдельную переменную 
 
 	try
 	{
@@ -38,11 +38,6 @@ std::optional<Content> CheckArgument(int argc, char* argv[])
 		std::cout << "Error: " << e.what() << ". Argument must be a number" << std::endl;
 		return std::nullopt;
 	}
-	catch (const std::out_of_range& e)
-	{
-		std::cout << "Error: " << e.what() << std::endl;
-		return std::nullopt;
-	}
 	catch (const std::exception& e)
 	{
 		std::cout << "Error: " << e.what() << std::endl;
@@ -50,7 +45,7 @@ std::optional<Content> CheckArgument(int argc, char* argv[])
 	}
 }
 
-int FlipByte(const uint8_t inputNumber)
+uint8_t FlipByte(const uint8_t inputNumber)
 {
 	uint8_t number = inputNumber;
 	uint8_t result = 0;
@@ -60,18 +55,18 @@ int FlipByte(const uint8_t inputNumber)
 		result |= (number & 1) << (7 - i);
 		number >>= 1;
 	}
-	return static_cast<int>(result);
+	return result;
 }
 
 int main(int argc, char* argv[])
 {
-	auto cont = CheckArgument(argc, argv);
+	auto args = ParsArgs(argc, argv);
 
-	if (!cont)
+	if (!args)
 	{
 		return 1;
 	}
-	std::cout << FlipByte(static_cast<uint8_t>(cont->number)) << std::endl;
+	std::cout << static_cast<int>(FlipByte(static_cast<uint8_t>(args->number))) << std::endl;
 
 	return 0;
 }
