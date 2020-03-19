18fa460212970914484d4c0f2ef325d1435308bb
MaxMartST <AdderGtx@gmail.com>
2020-03-19 22:03:51 +0300

    арх

--- Lw 1/lab1.4 rle/lab1.4 rle/main.cpp
+++ Lw 1/lab1.4 rle/lab1.4 rle/main.cpp
@@ -1,11 +1,11 @@
 #include "ParseArgument.h"
-#include "LinkerBinaryFile.h"
+#include "ArchiverBinaryFile.h"
 
 int main(int argc, char* argv[])
 {
 	if (auto args = ParseArgs(argc, argv))
 	{
-		return FileLinker(*args) ? 0 : 1;
+		return FileLArchiver(*args) ? 0 : 1;
 	}
 	else
 	{
