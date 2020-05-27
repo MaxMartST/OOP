439eb8958dc1b09ceea69f29e0018185ca86b1d1
MaxMartST <AdderGtx@gmail.com>
2020-05-27 12:07:04 +0300

    правил IsEqual

--- Lw 5/task_1.2/CompareTwoDouble.cpp
+++ Lw 5/task_1.2/CompareTwoDouble.cpp
@@ -3,6 +3,5 @@
 
 bool IsEqual(double x, double y)
 {
-	//return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
 	return boost::math::relative_difference(x, y) < std::numeric_limits<double>::epsilon();
 }
